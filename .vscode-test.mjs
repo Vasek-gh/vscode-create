dbd090c414d4bf441e4ef3b30df4b0238342d7aa
Vasek <my.super.g.email@gmail.com>
2025-06-30 19:05:27 +0300

    -

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,16 +1,38 @@
 import { defineConfig } from '@vscode/test-cli';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const filename = fileURLToPath(import.meta.url);
+const currentDir = dirname(filename);
+const fixedCwd = currentDir[0].toLowerCase() + currentDir.substring(1);
+
+console.log("##########################");
+console.log(fixedCwd);
 
 export default defineConfig({
-	files: "out/tests/**/*.tests.js",
-    workspaceFolder: "out/tests/project/project.code-workspace",
-    mocha: {
-        require: [ "./out/tests/MochaHooks.js" ],
-    },
+    tests: [{
+        files: "./out/tests/**/*.tests.js",
+        srcDir: fixedCwd,
+        workspaceFolder: "out/tests/project/project.code-workspace",
+    }],
     coverage: {
-        output: "./cov",
         includeAll: true,
         include: [
-            "out/src/**/*.js"
+            `${fixedCwd}/out/src/**/*.js`,
+        ],
+        exclude: [
+            `${fixedCwd}/out/src/api/*.*`,
+            `${fixedCwd}/out/src/context/*.*`,
+            `${fixedCwd}/out/src/extension.js`,
+            `${fixedCwd}/out/src/actions/Action.js`,
+            `${fixedCwd}/out/src/actions/ActionFactory.js`,
+            `${fixedCwd}/out/src/actions/ActionProvider.js`,
+            `${fixedCwd}/out/src/actions/ActionProviderFactory.js`,
+            `${fixedCwd}/out/src/actions/CommandAction.js`,
+            `${fixedCwd}/out/src/actions/SuggestionAction.js`,
+            `${fixedCwd}/out/src/configuration/ExtensionConfig.js`,
+            `${fixedCwd}/out/src/configuration/TemplateConfig.js`,
+            `${fixedCwd}/out/src/configuration/TemplateItemConfig.js`,
         ]
     }
 });
--- tests/MochaHooks.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode";
-import { TestsUtils } from "./TestsUtils";
-
-export async function mochaGlobalSetup(): Promise<void> {
-    // await TestsUtils.init();
-}
